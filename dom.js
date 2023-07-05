f12f539953ae39b28b6b28d973877c7fb0112a8d
befierce <enterbhatt123@gmail.com>
2023-07-05 19:38:54 +0530

    DOM assgnmnt 5 cpmlete

--- dom.js
+++ dom.js
@@ -12,6 +12,25 @@ for (let i = 0; i < list.length; i++) {
 }
 list[2].style.backgroundColor = 'yellow';
 var li = document.getElementsByTagName('li');
-li[4].textContent = 'new item';
-li[4].style.backgroundColor = '#D03D56';
-li[4].style.fontWeight = 'bold'
+// li[4].textContent = 'new item';
+// li[4].style.backgroundColor = '#D03D56';
+// li[4].style.fontWeight = 'bold';
+header.style.borderBottom = 'solid 5px #000'
+// li[1].style.backgroundColor = 'green';
+// li[2].style.display = 'none';
+var input = document.querySelector('#main-header');
+input.value = 'Hello World';
+
+var item = document.querySelector('.list-group-item');
+item.style.backgroundColor = 'red';
+
+var lst = document.querySelector('.list-group-item:last-child');
+lst.style.backgroundColor = 'blue';
+
+var second = document.querySelectorAll('.list-group-item');
+second[1].style.backgroundColor = 'green';
+for(let i = 0; i < list.length; i++){
+    if(i%2 != 0){
+        second[i].style.backgroundColor = 'green';
+    }
+}
