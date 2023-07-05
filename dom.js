e05865456061fc44b370db3fb9c253a375bb568e
befierce <enterbhatt123@gmail.com>
2023-07-05 12:26:19 +0530

    few deletion border and list item changes

---
+++ dom.js
@@ -0,0 +1,12 @@
+//document.title='123' 
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000'
+const title = document.getElementsByClassName("title");
+console.log(title);
+title[0].style.color = 'green';
+title[0].style.fontWeight = 'bold';
+const list = document.getElementsByClassName("list-group-item");
+for (let i = 0; i < list.length; i++) {
+    list[i].style.color = 'voilet';
+    list[i].style.fontWeight = 'bold';
+  }
